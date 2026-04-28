530a5f9e78c309bb8213cf0608a69aa3919814a7
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2026-04-28 20:13:12 +0700

    fix: duplicate image files got overwritten
    fix: crawl documents concurrently

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -10,32 +10,38 @@ const scrapeData = async (
   documentName: string,
   height = DEFAULT_IMAGE_HEIGHT,
   width?: number,
-): Promise<ImageData[]> => {
+): Promise<{
+  images: ImageData[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  manifest: any;
+}> => {
   try {
     const manifestUrl = `https://digi.vatlib.it/iiif/${documentName}/manifest.json`;
 
     const manifest = await fetch(manifestUrl).then((res) => res.json());
     logger.info(`Fetched manifest from ${manifestUrl}`);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const images = manifest.sequences[0].canvases.map((canvas: any) => {
-      const imageUrl =
-        canvas.thumbnail['@id']
-          .replace('thumb', 'iiif')
-          .replace('.tif.jpg', '.jp2') +
-        `/full/${height},${width || ''}/0/native.jpg`;
-      const imageName = canvas.label.replace(/\s+/g, '_') + '.jpg';
+    const images = manifest.sequences[0].canvases.map(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (canvas: any, idx: number) => {
+        const imageUrl =
+          canvas.thumbnail['@id']
+            .replace('thumb', 'iiif')
+            .replace('.tif.jpg', '.jp2') +
+          `/full/${height},${width || ''}/0/native.jpg`;
+        const imageName = `[${idx + 1}]_${canvas.label.replace(/\s+/g, '_')}.jpg`;
 
-      return {
-        url: imageUrl,
-        name: imageName,
-      } satisfies ImageData;
-    });
+        return {
+          url: imageUrl,
+          name: imageName,
+        } satisfies ImageData;
+      },
+    );
 
-    return images;
+    return { images, manifest };
   } catch (error) {
     logger.error(`Error fetching manifest for ${documentName}: ${error}`);
-    return [];
+    return { images: [], manifest: null };
   }
 };
 
